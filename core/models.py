ef6f0711b9504a46633199f189043a56f96e2936
Michele Memoli <michele@100shapes.com>
2015-02-11 22:02:25 +0000

    Added a content page

--- core/models.py
+++ core/models.py
@@ -1,7 +1,22 @@
 from django.db import models
-
+from wagtail.wagtailadmin.edit_handlers import FieldPanel
+from wagtail.wagtailcore.fields import RichTextField
 from wagtail.wagtailcore.models import Page
 
 
 class HomePage(Page):
-    pass
+    subpage_types = ['core.ContentPage', ]
+
+
+class ContentPage(Page):
+    subpage_types = ['core.ContentPage', ]
+
+    subtitle = models.CharField(max_length=255)
+    content = RichTextField()
+
+
+ContentPage.content_panels = [
+    FieldPanel('title', classname="full title"),
+    FieldPanel('subtitle', classname="full title"),
+    FieldPanel('content', classname="full"),
+]
