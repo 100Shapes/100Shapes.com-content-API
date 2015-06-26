dd768c0fec3eeaddf20d17168483216dcdc3b0fe
Michele Memoli <michele@100shapes.com>
2015-06-26 18:20:22 +0100

    Added model for service page

---
+++ services/migrations/0001_initial.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('wagtailcore', '0015_add_more_verbose_names'),
+        ('wagtailimages', '0006_add_verbose_names'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Service',
+            fields=[
+                ('page_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='wagtailcore.Page')),
+                ('body', models.TextField()),
+                ('lead', models.CharField(max_length=255)),
+                ('intro', models.CharField(max_length=255)),
+                ('is_featured', models.BooleanField(default=False)),
+                ('banner_image', models.ForeignKey(related_name='+', on_delete=django.db.models.deletion.SET_NULL, blank=True, to='wagtailimages.Image', null=True)),
+                ('thumbnail_image', models.ForeignKey(related_name='+', on_delete=django.db.models.deletion.SET_NULL, blank=True, to='wagtailimages.Image', null=True)),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=('wagtailcore.page',),
+        ),
+    ]
--- services/models.py
+++ services/models.py
@@ -1,3 +1,71 @@
 from django.db import models
 
-# Create your models here.
+from wagtail.wagtailcore.models import Page
+from wagtail.wagtailcore.fields import RichTextField
+from wagtail.wagtailadmin.edit_handlers import FieldPanel, InlinePanel, MultiFieldPanel
+from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
+
+from django.conf import settings
+
+
+###################
+
+# BLOG POSTS
+
+class Service(Page):
+    
+    body = models.TextField()
+    
+    lead = models.CharField(max_length=255)
+    intro = models.CharField(max_length=255)
+    
+    thumbnail_image = models.ForeignKey(
+        'wagtailimages.Image',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='+'
+    )
+
+    banner_image = models.ForeignKey(
+        'wagtailimages.Image',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='+'
+    )
+    
+    is_featured = models.BooleanField(default=False)
+
+    @property
+    def thumbnail_url(self):
+        return self.thumbnail_image.get_rendition('original').url
+
+    @property
+    def banner_url(self):
+        return self.banner_image.get_rendition('original').url
+
+
+    content_panels = Page.content_panels + [
+        FieldPanel('lead', classname="full"),
+        FieldPanel('intro', classname="full"),
+        FieldPanel('body', classname="full"),
+    ]
+
+    promote_panels = [
+        MultiFieldPanel(Page.promote_panels, "Common page configuration"),
+        FieldPanel('is_featured'),
+        ImageChooserPanel('thumbnail_image'),
+        ImageChooserPanel('banner_image'),
+    ]
+
+    api_fields = (
+        'thumbnail_url',
+        'banner_url',
+        'body',
+        'slug',
+        'lead',
+        'intro',
+        'seo_title',
+        'is_featured'
+    )
