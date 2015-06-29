a64cc2c0978392a407c63f1ecd7666d19a828a0e
Paolo Memoli <paolo@100shapes.com>
2015-06-29 18:18:32 +0100

    added images URL to generate previews

--- ohs/urls.py
+++ ohs/urls.py
@@ -7,8 +7,10 @@ from django.contrib import admin
 
 from wagtail.wagtailadmin import urls as wagtailadmin_urls
 from wagtail.wagtailsearch import urls as wagtailsearch_urls
+from wagtail.wagtailimages import urls as wagtailimages_urls
 from wagtail.wagtaildocs import urls as wagtaildocs_urls
 from wagtail.wagtailcore import urls as wagtail_urls
+
 
 from wagtailapi import urls as wagtailapi_urls
 
@@ -19,6 +21,7 @@ urlpatterns = patterns('',
     url(r'^admin/', include(wagtailadmin_urls)),
     url(r'^search/', include(wagtailsearch_urls)),
     url(r'^documents/', include(wagtaildocs_urls)),
+    url(r'^images/', include(wagtailimages_urls)),
 
     url(r'^api/', include(wagtailapi_urls)),
 
@@ -30,4 +33,3 @@ if settings.DEBUG:
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
     urlpatterns += staticfiles_urlpatterns()
-    urlpatterns += static(settings.MEDIA_URL + 'images/', document_root=os.path.join(settings.MEDIA_ROOT, 'images'))
