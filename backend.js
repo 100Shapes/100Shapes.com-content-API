00628b20f77c2f926e4cb06c61dbe4d77fbfffb9
Paolo Memoli <paolo@100shapes.com>
2015-07-28 16:36:19 +0100

    added limits and featured, closes #7 and closes #6

--- backend.js
+++ backend.js
@@ -1,43 +1,66 @@
+var Joi = require('joi');
+
 module.exports = function(server) {
-  server.route({
-      method: 'GET',
-      path: '/',
-      handler: function (request, reply) {
-        server.methods.getFolders(function(err, folders) {
-          if (err) {
-              console.log(err);
-          } else {
-              reply(folders).code(200);
-          }
-        })
-      }
-  });
-
-  server.route({
-    method: 'GET',
-    path: '/{folder_name}/{item_name}',
-    handler: function (request, reply) {
-      server.methods.getFolderItem(request.params.folder_name, request.params.item_name, function(err, item) {
-        if (err) {
-            console.log(err);
-        } else {
-            reply(item).code(200);
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: function(request, reply) {
+            server.methods.getFolders(function(err, folders) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    reply(folders).code(200);
+                }
+            })
         }
-      })
-    }
-  });
+    });
 
-  server.route({
-      method: 'GET',
-      path: '/{folder_name}',
-      handler: function (request, reply) {
-        server.methods.getFolder(request.params.folder_name, function(err, folder_items) {
-          if (err) {
-              console.log(err);
-          } else {
-              reply(folder_items).code(200);
-          }
-        })
-      }
-  });
+    server.route({
+        method: 'GET',
+        path: '/{folder_name}/{item_name}',
+        handler: function(request, reply) {
+            server.methods.getFolderItem(request.params.folder_name, request.params.item_name, function(err, item) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    reply(item).code(200);
+                }
+            })
+        },
+        config: {
+            validate: {
+                query: {
+                    limit: Joi.number().integer(),
+                    featured: Joi.boolean()
+                }
+            }
+        }
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/{folder_name}',
+        handler: function(request, reply) {
+            var requested = {
+                folder: request.params.folder_name,
+                limit: request.query.limit,
+                featured: request.query.featured
+            };
+            server.methods.getFolder(requested, function(err, folder_items) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    reply(folder_items).code(200);
+                }
+            })
+        },
+        config: {
+            validate: {
+                query: {
+                    limit: Joi.number().integer(),
+                    featured: Joi.boolean()
+                }
+            }
+        }
+    });
 }
--- methods.js
+++ methods.js
@@ -5,15 +5,21 @@ module.exports = function(server) {
     return next(null, _.keys(server.app.content));
   });
 
-  server.method('getFolder', function (folder_name, next) {
+  server.method('getFolder', function (requested, next) {
+    folder_name = requested.folder
     folder = {};
     folder[folder_name] = [];
     if(_.has(server.app.content, [folder_name])){
       server.app.content[folder_name].items.forEach(function(item) {
         item_meta = _.omit(item, server.app.ignoreFromList)
-        folder[folder_name].push(item_meta);
+        if (requested.featured==item_meta.featured) {
+            folder[folder_name].push(item_meta);
+        }
       });
     }
+    folder[folder_name] = folder[folder_name].reverse()
+    folder[folder_name] = folder[folder_name].slice(0,requested.limit);
+    _.sortBy(folder[folder_name],'date');
     return next(null, folder);
   });
 
