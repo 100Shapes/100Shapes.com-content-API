9145ebd7214cb5c45e93e62b35b25d8549e44ce9
Paolo Memoli <paolo@100shapes.com>
2015-07-30 12:47:53 +0100

    update content update script

--- update-content.sh
+++ update-content.sh
@@ -3,5 +3,7 @@ git clone https://github.com/100Shapes/100Shapes.com-content-API.git /tmp/app
 cp -r /tmp/app/* /home/app/
 rm -r /tmp/app
 cd /home/app/
+echo "Content Pulled. Running npm install"
 npm install
+echo "npm install complete. Tidying up"
 chown -R app /home/app/
