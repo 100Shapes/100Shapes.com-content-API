fd547ba033d7cd4110fe789a74ce3495b2259b7c
Paolo Memoli <paolo@100shapes.com>
2015-08-07 10:32:13 +0100

    updated pulling script

--- update-content.sh
+++ update-content.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
-echo "Clearing old Content from branch: ${BRANCH:-master}@${GIT_URL}"
+echo "Clearing old Content"
 rm -r home/app/content
+echo "Pulling content from branch: ${BRANCH:-master}@${GIT_URL}"
 git clone --depth 1 -b ${BRANCH:-master} ${GIT_URL}  /tmp/app
+echo "Transfering content"
 cp -r /tmp/app/* /home/app/
+echo "Tidying temp content"
 rm -r /tmp/app
 cd /home/app/
 echo "Content Pulled. Running npm install"
 npm install
-echo "npm install complete. Tidying up"
+echo "npm install complete. Fixing permissions"
 chown -R app /home/app/
