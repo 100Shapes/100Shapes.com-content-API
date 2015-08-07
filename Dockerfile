07e9f6c7960d59c0381792302b441b1cd9887411
Paolo Memoli <paolo@100shapes.com>
2015-08-07 11:46:21 +0100

    updated scripts

--- Dockerfile
+++ Dockerfile
@@ -12,24 +12,21 @@ RUN /pd_build/utilities.sh
 RUN /pd_build/python.sh
 RUN /pd_build/nodejs.sh
 
+RUN apt-get update && apt-get install -y -o Dpkg::Options::="--force-confold" passenger nginx-extras
 RUN curl -sLo /usr/local/bin/ep https://github.com/kreuzwerker/envplate/releases/download/v0.0.7/ep-linux && chmod +x /usr/local/bin/ep
 
 RUN npm install -g npm
 
 RUN mkdir -p /etc/my_init.d
-
 ADD update-content.sh /etc/my_init.d/update-content.sh
 
 ENV BRANCH staging
 ENV GIT_URL https://github.com/100Shapes/100Shapes.com-content-API.git
 
-RUN ep /etc/my_init.d/update-content.sh
-
-RUN /etc/my_init.d/update-content.sh
-
 ENV VIRTUAL_HOST stage.api.100shapes.com
 ENV FRONTEND_URL http://100shapes.com
 
+RUN ["/etc/my_init.d/update-content.sh", "fresh"]
 
 # Enable nginx
 RUN rm -f /etc/service/nginx/down
--- update-content.sh
+++ update-content.sh
@@ -1,14 +1,27 @@
 #!/bin/sh
-echo "Clearing old Content"
-rm -r home/app/content
-echo "Pulling content from branch: ${BRANCH:-master}@${GIT_URL}"
-git clone --depth 1 -b ${BRANCH:-master} ${GIT_URL}  /tmp/app
-echo "Transfering content"
-cp -r /tmp/app/* /home/app/
-echo "Tidying temp content"
-rm -r /tmp/app
+echo "Injecting Enviroment Variables"
+# ep /etc/nginx/sites-enabled/nginx.conf
+ep /etc/my_init.d/update-content.sh
+
+if [ $1="fresh" ]; then
+    rm -r /home/app
+    echo "Cloning content from branch: ${BRANCH:-master}@${GIT_URL}"
+    git clone --depth 1 -b ${BRANCH:-master} ${GIT_URL} /home/app
+else
+    cd /home/app/
+    echo "Clearing old Content"
+    rm -r home/app/content
+    echo "Pulling content from branch: ${BRANCH:-master}@${GIT_URL}"
+    git pull
+
+fi
+
+echo "Running npm install"
 cd /home/app/
-echo "Content Pulled. Running npm install"
 npm install
-echo "npm install complete. Fixing permissions"
+
+# echo "Running webpack"
+# webpack -p
+
+echo "Fixing permissions"
 chown -R app /home/app/
