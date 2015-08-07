cc1cfee8df77836949353c9bfe5aa115ca8aff86
Paolo Memoli <paolo@100shapes.com>
2015-08-07 12:05:16 +0100

    completed update script

--- update-content.sh
+++ update-content.sh
@@ -3,17 +3,15 @@ echo "Injecting Enviroment Variables"
 # ep /etc/nginx/sites-enabled/nginx.conf
 ep /etc/my_init.d/update-content.sh
 
-if [ $1="fresh" ]; then
+if [ $1 = "fresh" ]; then
+    echo "Doing fresh clone"
     rm -r /home/app
     echo "Cloning content from branch: ${BRANCH:-master}@${GIT_URL}"
     git clone --depth 1 -b ${BRANCH:-master} ${GIT_URL} /home/app
 else
     cd /home/app/
-    echo "Clearing old Content"
-    rm -r home/app/content
     echo "Pulling content from branch: ${BRANCH:-master}@${GIT_URL}"
     git pull
-
 fi
 
 echo "Running npm install"
